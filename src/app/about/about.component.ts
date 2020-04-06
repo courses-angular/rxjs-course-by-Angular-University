f22494be0ac3fbf87e3136a37a12240e1eb63786
yevgenyl <yevgeny@consist.co.il>
2020-04-06 14:05:32 +0300

    add project

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,34 +1,76 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject
 } from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+  ngOnInit() {
+    // Using Vanilla JS
+    // document.addEventListener('click', event => {
+    //   console.log(event);
+    //   setTimeout(() => {
+    //     console.log('Finished...');
+    //     let counter = 0;
+    //
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
+    //  ==================  Using Rxjs ===========================
+
+    // Interval
+    const interval$ = interval(1000);
+
+    // interval$.subscribe(value => console.log('stream-1', value));
+    // interval$.subscribe(value => console.log('stream-2', value));
+
+    // Unsubscribe the interval
+    const sub = interval$.subscribe(value => console.log('stream-2', value));
+
+    // Unsubscribe after 5s
+    setTimeout(() => sub.unsubscribe(), 5000);
 
 
-    }
+    // Timer
+    const timer$ = timer(3000, 1000);
+    // timer$.subscribe(value => console.log('stream-3', value));
+
+    // FromEvent
+    const click$ = fromEvent(document, 'click');
+    // click$.subscribe(event => console.log('Clicks', event));
+
+
+    // With error and complete
+    click$.subscribe(
+      event => console.log('Clicks', event),
+      error => console.log(error),
+      () => console.log('Completed...')
+    );
+  }
 
 
 }
