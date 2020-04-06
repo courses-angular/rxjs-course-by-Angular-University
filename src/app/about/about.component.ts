465fad7f08b8b756441f1c8cff1b821b46cc3da6
yevgenyl <yevgeny@consist.co.il>
2020-04-06 14:07:39 +0300

    ngOnInit in Master in About Page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -25,10 +25,52 @@ import {createHttpObservable} from '../common/util';
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+  ngOnInit() {
+    // Using Vanilla JS
+    // document.addEventListener('click', event => {
+    //   console.log(event);
+    //   setTimeout(() => {
+    //     console.log('Finished...');
+    //     let counter = 0;
+    //
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
+    //  ==================  Using Rxjs ===========================
+
+    // Interval
+    const interval$ = interval(1000);
+
+    // interval$.subscribe(value => console.log('stream-1', value));
+    // interval$.subscribe(value => console.log('stream-2', value));
+
+    // Unsubscribe the interval
+    const sub = interval$.subscribe(value => console.log('stream-2', value));
+
+    // Unsubscribe after 5s
+    setTimeout(() => sub.unsubscribe(), 5000);
 
 
-    }
+    // Timer
+    const timer$ = timer(3000, 1000);
+    // timer$.subscribe(value => console.log('stream-3', value));
+
+    // FromEvent
+    const click$ = fromEvent(document, 'click');
+    // click$.subscribe(event => console.log('Clicks', event));
+
+
+    // With error and complete
+    click$.subscribe(
+      event => console.log('Clicks', event),
+      error => console.log(error),
+      () => console.log('Completed...')
+    );
+  }
 
 
 }
